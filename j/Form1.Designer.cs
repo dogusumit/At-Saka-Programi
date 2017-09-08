8b62da8ad04a7d48a1947e4b6e7cdf915917df9d
dogusumit <31733984+dogusumit@users.noreply.github.com>
2017-09-08 13:34:29 +0300

    Add files via upload

---
+++ j/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace j
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(285, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(300, 200);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.TopMost = true;
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseEnter += new System.EventHandler(this.Form1_MouseEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+
+
+    }
+}
+
---
+++ j/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading; 
+using System.IO;
+using System.Drawing;
+using System.Media;
+namespace j
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        { InitializeComponent(); }
+   
+
+
+        int koor_x = Screen.PrimaryScreen.Bounds.Width ;
+        int koor_y = Screen.PrimaryScreen.Bounds.Height ;
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
+        public string Pcommand;
+        string yol = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).ToString() + "\\dittiri.mp3";
+        SoundPlayer player = new SoundPlayer();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bool TekProcess;
+            Mutex mx = new Mutex(true, "Muttexyeah", out TekProcess);
+            if (!TekProcess)
+            {
+                MessageBox.Show("Başka bir kopya çalıştıramazsınız");
+                Application.ExitThread();
+
+            }
+                player.Stream = Properties.Resources.kisne;
+                this.Location = new Point(10, 10);
+                
+        }
+
+        /*
+        bool a = true;
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (a)
+                e.Cancel = true;
+
+        }
+  private string tuslar="";
+          private void Form1_KeyDown(object sender, KeyEventArgs e)
+          {
+              if (e.KeyCode == Keys.Enter)
+              {
+                  if (tuslar == "QUIT")
+                  {
+                      try
+                      {
+                          tuslar = "";
+                          DialogResult = MessageBox.Show("Korktun mu :D", "Hack Bitecek!", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                          if (DialogResult == DialogResult.Yes)
+                          {
+                              a = false;
+                             
+                              Application.Exit();
+                          }
+                      }
+                      catch { }
+
+                  }
+                  else
+                  {
+                      tuslar = "";
+                  }
+              }
+              else
+              {
+                  tuslar += e.KeyCode;
+              }
+
+
+        
+
+          }*/
+        private void Form1_MouseEnter(object sender, EventArgs e)
+        {
+           
+                Random f1 = new Random();
+                this.Location = new Point(Convert.ToInt32(f1.Next(10,koor_x-300)), Convert.ToInt32(f1.Next(10,koor_y-200)));
+                player.Play();
+
+        }
+    }
+}
